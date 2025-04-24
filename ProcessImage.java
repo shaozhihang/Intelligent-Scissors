7af56ecb1fe815b9ea39ea67b625f031ca9e3fc7
shiYifan <2578260985@qq.com>
2025-04-24 17:54:49 +0800

    新增了ProcessImage工具类与ImageGraph对象类

--- ProcessImage.java
+++ ProcessImage.java
@@ -1,3 +1,8 @@
+import java.awt.image.DataBufferInt;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
 * 图像转换为图结构
 每个像素视为图中的一个节点，每个节点与其8邻域像素通过“链接”连接。
@@ -8,6 +13,38 @@
 链接成本与梯度成反比：梯度越高（边缘越强），成本越低。
 * */
 public class ProcessImage {
+    /// 把图像转换为像素点矩阵
+    /// @param image 原始图片对象
+    /// @return 二维int类型数组，表示传入图片的像素点矩阵
+    public static int[][] toRGBMatrix(Image image){
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[][] pixelMatrix = new int[width][height];
+
+        // 获取像素数据的一维数组
+        int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+        // 将一维数组转换为二维矩阵（按行优先存储）
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int index = y * width + x;
+                pixelMatrix[x][y] = pixels[index];
+            }
+        }
+        return pixelMatrix;
+    }
+
+    /**
+     * 把图像转换为图结构
+     * @param image 要处理的Image类型图像
+     * @return 一个包含节点与其所连接的边们的映射列表
+     */
+    public static Map<Node, List<Edge>> toGraph(Image image) {
+        return ImageGraph.buildGraph(image);
+    }
+
+
+
 
 }
 
