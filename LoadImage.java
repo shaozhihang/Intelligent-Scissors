fb016217c7801c35bfed42653903924c7e54f68f
shiYifan <2578260985@qq.com>
2025-04-24 16:06:35 +0800

    新增可视窗口与main类

--- LoadImage.java
+++ LoadImage.java
@@ -1,7 +1,15 @@
 /*
 * 从文件夹读取图片文件，转化为java中的对象
 * */
+import javax.imageio.ImageIO;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 public class LoadImage {
-
+    public Image load(String imgPathStr) throws IOException {
+        String[] pPath = imgPathStr.split("\\.");
+        String imgType = pPath[pPath.length-1];
+        return new Image(imgType, ImageIO.read(new FileInputStream(imgPathStr)));
+    }
 }
